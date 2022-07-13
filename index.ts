96dcbea1f0ed2ac28e79efb4366c37274937f0e7
Artur Mozdzonek <mojon1979@gmail.com>
2022-07-14 01:23:55 +0200

    Added rate-limiter.

--- index.ts
+++ index.ts
@@ -1,7 +1,9 @@
 import express, { json } from 'express';
 import cors from 'cors';
+import rateLimit from 'express-rate-limit';
 import { handleError } from './utils/error';
 import { config } from './config/config';
+import { productRouter } from './routers/productRouter';
 
 const app = express();
 
@@ -12,7 +14,14 @@ app
     })
   )
   .use(json())
+  .use(
+    rateLimit({
+      windowMs: 5 * 60 * 1000, // 5 minutes
+      max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+    })
+  )
 
+  .use('/product', productRouter)
   .use(handleError)
   .listen(3001, 'localhost', () => {
     console.log('Listening on http://localhost:3001');
