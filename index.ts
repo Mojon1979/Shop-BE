a2c0e04eca184ac20dbf24d9f35da5c51969c05a
Artur Mozdzonek <mojon1979@gmail.com>
2022-07-13 20:53:17 +0200

    First commit.

---
+++ index.ts
@@ -0,0 +1,15 @@
+import express, {json} from "express";
+import cors from "cors";
+
+const app = express();
+
+app
+  .use(cors({
+    origin: 'http://localhost:3000',
+  }))
+  .use(json())
+  .listen(3001, 'localhost', () => {
+    console.log('Listening on http://localhost:3001')
+  })
+
+
